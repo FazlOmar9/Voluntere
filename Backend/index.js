cae65a96ad487e08dec8cf7515a03a345f3583cc
FazlOmar9 <bt22csa026@iiitn.ac.in>
2024-03-29 08:28:49 +0530

    Removed redundant code from frontend

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/voluntere', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
